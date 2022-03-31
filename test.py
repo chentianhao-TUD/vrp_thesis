535e1884a42f7afd6f6b577e7cac47722a2ff8a9
maple <chen.tianhao@icloud.com>
2022-03-31 13:35:56 +0200

    31MAR

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import numpy as np
 Nodes = [0, 1, 2, 3]
 distance = {(0, 1): 63, (1, 0): 63, (0, 2): 67, (2, 0): 67, (1, 2): 50, (2, 1): 50, (0, 3): 100, (3, 0): 100, (1, 3): 162, (3, 1): 162, (2, 3): 161, (3, 2): 161}
 demand = [0, 300, 500, 700]
+Vehicle = [0, 1]
 # [end input data]
 
 # [start initiate model and solver]
@@ -13,36 +14,62 @@ model = cp_model.CpModel()
 solver = cp_model.CpSolver()
 # [end initiate model and solver]
 
-k = {}
+single_K = {}
+any_k = {}
+time_back_to_depot = {}
+start_time = {}
+service_time = demand
+
+
+for k in Vehicle:
+    for i in Nodes:
+        start_time[i] = model.NewIntVar(0, 9999, f'start time at node{i}')
+        for j in Nodes:
+            single_K[k, i, j] = model.NewBoolVar(f'v{k} {i} -> {j}')
+            any_k[i, j] = model.NewBoolVar(f'v {i} -> {j}')
+    time_back_to_depot[k] = model.NewIntVar(0, 9999, f'v{k} back to depot')
+
+for k in Vehicle:
+    for i in Nodes:
+        for j in Nodes:
+            model.AddImplication(single_K[k, i, j], any_k[i, j])
+            model.AddImplication(any_k[i, j].Not(), single_K[k, i, j].Not())
+
 for i in Nodes:
     for j in Nodes:
-        k[i, j] = model.NewBoolVar(f'{i} --> {j}')
+        if i == j:
+            model.Add(any_k[i, j] == 0)
 
-M = 99999
+for j in Nodes:
+    model.AddAtLeastOne(single_K[k, i, j] for k in Vehicle for i in Nodes)
 
-start_service = {}
+for k in Vehicle:
+    model.AddExactlyOne(single_K[k, i, 0] for i in Nodes)
+
 for i in Nodes:
-    start_service[i] = model.NewIntVar(0, 9999, f'start time at node{i}')
-model.Add(start_service[0] == 0)
+    for j in Nodes:
+        for k in Vehicle:
+            model.AddExactlyOne(single_K[k, i, j]).OnlyEnforceIf(single_K[k, i, h])
 
-for i in Nodes:
-    for j in Nodes[1:]:
-        if i != j:
-            start_service[j] = start_service[i] + demand[i] + distance[i, j] - (k[i, j] - 1) * M
+model.Add(start_time[0] == 0)
 
+for k in Vehicle:
+    for i in Nodes[1:]:
+        model.Add(time_back_to_depot[k] >= start_time[i] + service_time[i] + distance[i, 0]).OnlyEnforceIf(
+            single_K[k, i, 0])
 
-t_list = []
-for key, value in start_service:
-
-obj = model.NewIntVar(0, 9999, 'max')
-model.AddMaxEquality(obj, start_service.values())
-
-
+obj = model.NewIntVar(0, 9999, 'obj')
+model.AddMaxEquality(obj, time_back_to_depot.values())
 model.Minimize(obj)
+
 status = solver.Solve(model)
-if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+if status == cp_model.OPTIMAL:
     print(solver.ObjectiveValue())
     for i in Nodes:
         for j in Nodes:
-            if solver.Value(k[i, j]) and solver.Value(start_service[i]):
-                print(i, j, solver.Value(start_service[i]))
+            for k in Vehicle:
+                if solver.Value(single_K[k, i, j]):
+                    print(single_K[k, i, j])
+else:
+    print('failed')
+
